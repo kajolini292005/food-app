020826480894ed4bdfef1905540d7f2ba80f0994
Kajolini Palaniswamy <72745032+kajolini292005@users.noreply.github.com>
2026-03-17 23:28:24 +0530

    Add files via upload

---
+++ food-sharing-app/app.py
@@ -0,0 +1,70 @@
+from flask import Flask, render_template, request, redirect
+import pymysql
+import os
+from werkzeug.utils import secure_filename
+
+connection = pymysql.connect(
+    host="foodsharingmysql.cixkummayyzb.us-east-1.rds.amazonaws.com",
+    user="kajolini",
+    password="Food12345",
+    database="foodapp"
+)
+
+cursor = connection.cursor()
+
+cursor.execute("SELECT * FROM foods")
+
+rows = cursor.fetchall()
+
+for row in rows:
+    print(row)
+
+app = Flask(__name__)
+
+food_items = []
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+UPLOAD_FOLDER = "static/uploads"
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+@app.route('/upload', methods=['GET','POST'])
+def upload():
+    if request.method == 'POST':
+
+        food = request.form['food']
+        quantity = request.form['quantity']
+        location = request.form['location']
+
+        image = request.files['image']
+
+        filename = secure_filename(image.filename)
+
+        image_path = os.path.join("static/uploads", filename)
+
+        image.save(image_path)
+
+        cursor.execute(
+            "INSERT INTO foods(food_name, quantity, location, image) VALUES (%s,%s,%s,%s)",
+            (food, quantity, location, filename)
+        )
+
+        connection.commit()
+
+        return redirect('/foods')
+
+    return render_template("upload.html")
+
+@app.route('/foods')
+def foods():
+    cursor.execute("SELECT * FROM foods")
+    foods = cursor.fetchall()
+    return render_template("food_list.html", foods=foods)
+
+if __name__ == "__main__":
+    app.run(debug=True)
